f7c79cf626bf4fbd033d38687084d6bf9f071a76
Artem <demchkoa@gmail.com>
2022-07-06 21:41:30 +0300

    'update'

--- script.js
+++ script.js
@@ -5,8 +5,17 @@ document.querySelector('button').addEventListener('click', () => {
     if(inputEng, inputTranslated){
         const newBlock = document.createElement('div');
         const btnDel = document.createElement('button');
+
         newBlock.classList.add('block');
         newBlock.textContent = inputEng;
+        btnDel.textContent = 'X';
+        btnDel.classList.add('remove');
+        newBlock.append(btnDel);
+
+        btnDel.addEventListener('click', e => {
+            e.stopPropagation();
+            newBlock.style.display = 'none'
+        })
 
         document.querySelector('.eng').value = '';
         document.querySelector('.translated').value = '';
@@ -15,12 +24,38 @@ document.querySelector('button').addEventListener('click', () => {
             if(!newBlock.classList.contains('active')){
                 newBlock.classList.add('active');
                 newBlock.textContent = inputTranslated;
+                newBlock.append(btnDel);
             } else {
                 newBlock.classList.remove('active');
                 newBlock.textContent = inputEng;
+                newBlock.append(btnDel);
             }
         });
 
         document.querySelector('.container').append(newBlock)
+    } else {
+        alert('Type something')
     }
-})
+});
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
